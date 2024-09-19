ede1cdc0af416c277a014066c088a8e8c92f3b2a
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2024-09-19 13:54:04 +0400

    Compute ray visibility (with offset)

--- Raytrace/src/src/main.rs
+++ Raytrace/src/src/main.rs
@@ -58,18 +58,18 @@ struct Sphere
     albedo: Vec3
 }
 
-struct Rayon
+struct Ray
 {
     origin: Vec3,
     direction: Vec3
 }
 
-fn get_intersection_distance(ray: &Rayon, t:f32) -> f32
+fn get_intersection_distance(ray: &Ray, t:f32) -> f32
 {
    t * length(&ray.direction)
 }
 
-fn get_intersection_point_t(ray: &Rayon, t:f32) -> Vec3
+fn get_intersection_point_t(ray: &Ray, t:f32) -> Vec3
 {
    add_vector(&ray.origin, &mul_scalar_vector(t, &ray.direction))
 }
@@ -82,7 +82,7 @@ struct Intersection
     albedo: Vec3
 }
 
-fn intersect_sphere(ray: &Rayon, sphere: &Sphere) -> Option<Intersection>
+fn intersect_sphere(ray: &Ray, sphere: &Sphere) -> Option<Intersection>
 {
     match intersect_sphere_t(&ray, &sphere)
     {
@@ -96,7 +96,7 @@ fn intersect_sphere(ray: &Rayon, sphere: &Sphere) -> Option<Intersection>
     }
 }
 
-fn intersect_spheres(ray: &Rayon, spheres: &Vec<Sphere>) -> Option<Intersection>
+fn intersect_spheres(ray: &Ray, spheres: &Vec<Sphere>) -> Option<Intersection>
 {
     let mut currentItM = None;
 
@@ -123,7 +123,7 @@ fn intersect_spheres(ray: &Rayon, spheres: &Vec<Sphere>) -> Option<Intersection>
     currentItM
 }
 
-fn intersect_sphere_t(ray: &Rayon, sphere: &Sphere) -> Option<f32>
+fn intersect_sphere_t(ray: &Ray, sphere: &Sphere) -> Option<f32>
 {
     let oc = subtract_vector(&ray.origin, &sphere.center);
 
@@ -192,9 +192,9 @@ fn main() {
          Sphere{radius: 50000.0, center: Vec3{x: 0.0, y: 50000.0 + 800.0, z: 0.0}, albedo: Vec3{x: 1.0, y: 1.0, z: 1.0}},
        ];
     let lights = vec![
-          Light{origin: Vec3{x: 1000.0, y: 0.0, z: 200.0}, emission: Vec3{x: 200000.0, y:200000.0, z:200000.0}},
-          Light{origin: Vec3{x: 1.0, y: -1000.0, z: 200.0}, emission: Vec3{x: 100000.0, y:0.0, z:0.0}},
-          Light{origin: Vec3{x: -1000.0, y: 1000.0, z: 200.0}, emission: Vec3{x: 0.0, y:100000.0, z:0.0}}
+          Light{origin: Vec3{x: 5000.0, y: 0.0, z: 0.0}, emission: Vec3{x: 400000.0, y:400000.0, z:400000.0}},
+          Light{origin: Vec3{x: 1.0, y: -1000.0, z: 0.0}, emission: Vec3{x: 100000.0, y:0.0, z:0.0}},
+          Light{origin: Vec3{x: -1000.0, y: 1000.0, z: 0.0}, emission: Vec3{x: 0.0, y:100000.0, z:0.0}}
          ];
 
     let scene = Scene{lights, spheres};
@@ -215,7 +215,7 @@ fn main() {
             let focal_point = Vec3{x:0.0, y:0.0, z: -focal};
             let direction = subtract_vector(&pixel, &focal_point);
 
-            let ray = Rayon {
+            let ray = Ray {
                 // We start from the screen and note the "focal point"
                 origin: pixel,
                 direction
@@ -235,9 +235,35 @@ fn main() {
                      let light_distance = length(&to_light);
                      let cos = (dot(&normalize(&to_light), &it.normal)).clamp(0.0, 1.0);
 
+                     // This is the amount of light, but not taking into account the visibilty
                      let v = mul_vector(&mul_scalar_vector(cos / light_distance, &it.albedo), &light.emission);
 
-                     contrib = add_vector(&contrib, &v);
+                     // Visibility
+                     // We shoot a ray toward the light
+                     let shadow_direction = normalize(&to_light);
+                     // Let's add a small offset to the origin point of the ray, so it won't
+                     // intersect the surface we are on.
+                     let origin_with_delta = add_vector(&it.point, &mul_scalar_vector(0.1, &shadow_direction));
+                     let shadow_ray = Ray{origin: origin_with_delta, direction: shadow_direction};
+            
+                     // compute the intersection
+                     // NOTE: this could be optimised because we don't care about the FIRST
+                     // intersection. Instead we want to find any intersection between the point
+                     // and the light. So we can implement an early exit. We also don't care about
+                     // material / normal / ... at intersection point.
+                     let it_shadow = intersect_spheres(&shadow_ray, &scene.spheres);
+
+                     let visibility = match it_shadow
+                        {
+                            // No intersection: full visibility
+                            None => 1.0,
+                            // Intersection, we check that the intersection is between us and the
+                            // light
+                            Some(it) => if it.distance > light_distance
+                                { 1.0 } else { 0.0 }
+                        };
+
+                     contrib = add_vector(&mul_scalar_vector(visibility, &v), &contrib);
                    }
                 }
                 None => 
